5eec76062ee86e8a1d58a73276ee8590f7107cf0
Martin Gabriel Brazon Betancourt <martin.betancourt@teclogica.com.br>
2026-05-27 14:49:39 -0300

    style:

--- src/execute.py
+++ src/execute.py
@@ -2,7 +2,7 @@ from environment_data import EnvironmentData
 from compare_tables import CompareTables
 from generate_report import GenerateReport
 
-GENERATE_REPORT = False;
+GENERATE_REPORT = False
 
 FILE_CSV_PROD = 'AT_PREPARE_STEP_PROD' 
 FILE_CSV_ACP = 'AT_PREPARE_STEP_ACP'
@@ -21,9 +21,9 @@ print(f"- Qty exclusivo em Prod: {len(exclusivos_prod)}")
 print(f"- Qty exclusivo em ACP: {len(exclusivos_acp)}")
 
 if GENERATE_REPORT:
-    print("Gerando Relatorio...")
-    prefix_file_name = '_'.join(FILE_CSV_PROD.split('_')[:-1])
-    relatorio = GenerateReport(exclusivos_prod, exclusivos_acp, prefix_file_name)
-    relatorio.generate_excel()
+  print("Gerando Relatorio...")
+  prefix_file_name = '_'.join(FILE_CSV_PROD.split('_')[:-1])
+  relatorio = GenerateReport(exclusivos_prod, exclusivos_acp, prefix_file_name)
+  relatorio.generate_excel()
 
 print("Análise concluída com sucesso!")
