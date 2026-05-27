f313a0f349c90207eb1cbdb98f3bd4d14b28d629
Martin Gabriel Brazon Betancourt <martin.betancourt@teclogica.com.br>
2026-05-27 14:39:28 -0300

    refactor: only use execute and deleted main class

--- src/execute.py
+++ src/execute.py
@@ -1,14 +1,29 @@
-from main import Main
+from environment_data import EnvironmentData
+from compare_tables import CompareTables
+from generate_report import GenerateReport
+
+GENERATE_REPORT = False;
 
 FILE_CSV_PROD = 'AT_PREPARE_STEP_PROD' 
 FILE_CSV_ACP = 'AT_PREPARE_STEP_ACP'
+CSV_PATH = r'..\data_csv'
 
-main = Main(FILE_CSV_PROD, FILE_CSV_ACP)
-data_prod, data_acp = main.load_data()
-exclusives_prod_len, exclusives_acp_len = main.compare_data_len()
-exclusives_prod, exclusives_acp = main.compare_data()
+print("Carregando arquivos...")
+data_prod = EnvironmentData('PROD', CSV_PATH, FILE_CSV_PROD).load_data()
+data_acp = EnvironmentData('ACP', CSV_PATH, FILE_CSV_ACP).load_data()
 
-main.generate_excel(exclusives_prod, exclusives_acp)
+print("Cruzando dados...")
+auditor = CompareTables(data_prod, data_acp, key_column='ID')
+exclusivos_prod = auditor.get_exclusives_prod()
+exclusivos_acp = auditor.get_exclusives_acp()
 
-print(f"Exclusives in PROD: {exclusives_prod}")
-print(f"Exclusives in ACP: {exclusives_acp}")
+print(f"- Qty exclusivo em Prod: {len(exclusivos_prod)}")
+print(f"- Qty exclusivo em ACP: {len(exclusivos_acp)}")
+
+if GENERATE_REPORT:
+    print("Gerando Relatorio...")
+    prefix_file_name = '_'.join(FILE_CSV_PROD.split('_')[:-1])
+    relatorio = GenerateReport(exclusivos_prod, exclusivos_acp, prefix_file_name)
+    relatorio.generate_excel()
+
+print("Análise concluída com sucesso!")
--- src/generate_report.py
+++ src/generate_report.py
@@ -1,13 +1,13 @@
 import pandas as pd 
 
 class GenerateReport:
-  def __init__(self, exclusives_prod: list[dict], exclusives_acp: list[dict], prefixFileName: str):
+  def __init__(self, exclusives_prod: pd.DataFrame, exclusives_acp: pd.DataFrame, prefixFileName: str):
     self.exclusives_prod = exclusives_prod
     self.exclusives_acp = exclusives_acp
     self.prefixFileName = prefixFileName
-    self.file_name = 'exclusives_' + prefixFileName + '.xlsx'
+    self.file_name = f'exclusives_{prefixFileName}.xlsx'
 
   def generate_excel(self):
     with pd.ExcelWriter(self.file_name) as writer:
-      self.exclusives_prod.to_excel(writer, sheet_name=f'Falta em ACP (Exclusivo Prod)', index=False)
-      self.exclusives_acp.to_excel(writer, sheet_name=f'Falta em Prod (Exclusivo ACP)', index=False)
+      self.exclusives_prod.to_excel(writer, sheet_name=f'Falta em ACP', index=False)
+      self.exclusives_acp.to_excel(writer, sheet_name=f'Falta em Prod', index=False)
