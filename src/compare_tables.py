062f8d1a55157c721243c9c92622dc5626eca6e4
Martin Gabriel Brazon Betancourt <martin.betancourt@teclogica.com.br>
2026-05-27 15:13:34 -0300

    feat: add method to detect differences

--- src/compare_tables.py
+++ src/compare_tables.py
@@ -8,6 +8,22 @@ class CompareTables:
 
     self.df_left = pd.merge(self.data_prod, self.data_acp, on=self.key_column, how='left', suffixes=('_PROD', '_ACP'), indicator=True)
     self.df_right = pd.merge(self.data_prod, self.data_acp, on=self.key_column, how='right', suffixes=('_PROD', '_ACP'), indicator=True)
+    self.df_both = pd.merge(self.data_prod, self.data_acp, on=self.key_column, how='both', suffixes=('_PROD', '_ACP'), indicator=True)
+
+  def get_both_differences(self):
+    df_both = self.df_left[self.df_left['_merge'] == 'both'].copy()
+    
+    base_columns = [col.replace('_PROD', '') for col in df_both.columns if col.endswith('_PROD')]
+    has_changes = False
+    for col in base_columns:
+      # Compara se a coluna PROD é diferente da coluna ACP correspondente
+      diverge = df_both[f'{col}_PROD'] != df_both[f'{col}_ACP']
+      has_changes = has_changes | diverge
+    
+    df_divergencias = df_both[has_changes].copy() # copia apenas as linhas que tem divergências
+    df_divergencias.drop(columns=['_merge'], inplace=True)
+    
+    return df_divergencias
 
   def get_exclusives_prod(self):
     # Filtro apenas os exclusivos
